8dfc98d43705896b000bc20eb91c30e066df614a
rajranerr <raneraj71@gmail.com>
2026-05-16 15:45:16 +0530

    add programs

---
+++ modules and comments.py
@@ -0,0 +1,18 @@
+# Modules and Comments
+
+# Modules: module is simply a file containimg python code definitions, functions, classes, and executable statements. Any python file saved with a .py extention is automatically treated as a module.
+
+# Types of modules
+
+# 1. built-in modules
+# 2. external modules 
+
+# Comments:
+#  # : for syntax highlighting   
+# for example: # I am raj  
+     
+#  """ -//- """ : for multi line comment
+# for ex:
+
+a = """"my name is raj and i am from haidarpur madhya pradesh"""
+print(a)

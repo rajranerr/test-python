3fa2bc1a33f94e766f21392b83a183a0d6f15a8d
rajranerr <raneraj71@gmail.com>
2026-05-16 16:04:39 +0530

    add new file

---
+++ Escape sequences.py
@@ -0,0 +1,29 @@
+# Escape sequences:
+
+# \n : for chenging on new line
+# for ex:
+text = "name: Raj Rane \n age: 21 "
+print(text)
+
+# \" -//- \" : the charactor want to insert
+# for ex:
+text = "hey i am \"like anyone\" but i can not say her "
+print(text)
+
+# \' -//- \' : in single line to insert single quote
+# for ex:
+text = 'she also like\'s me but she also can not say mes'
+print(text)
+# print statemant:
+
+# sep ( "," ):  for seprete a words 
+# example:
+print("Hey", 6, 7, 8, sep="-")
+# end ("end"): specific want at the end. Default is '\n' (line feed)
+# example :
+print("date of birth", 16,7,2005, sep="/" , end="raj rane\n")
+print("hello")
+
+# file:
+
+# object:

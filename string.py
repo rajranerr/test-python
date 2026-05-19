cf9c49776e1a244ea9b9d5eb26d580d99405b441
rajranerr <raneraj71@gmail.com>
2026-05-19 15:35:00 +0530

    add new program

---
+++ string.py
@@ -0,0 +1,34 @@
+# string: a string is a sequence of characters used to store and represent text.
+#        It is one of the most fundamental
+#       data types in the language and is represented by the str class.
+
+# Immutable: Once created, a string's content cannot be changed. Any modification (like appending or replacing) results in the creation of a completely new string.
+# Sequence Type: Strings are ordered, meaning each character has a specific position (index).
+# Unicode Support: Python strings are sequences of Unicode characters, allowing them to represent text from almost any language.
+
+# creating strings:
+
+# you can creat string by enclosed text in quots:
+
+# single quotes: 'hello'
+# double quotes: "hello"
+# triple quotes: """multi-line text""" or '''multi-line text'''(useful for text spanning several lines).
+
+# Common Operations:
+
+# python provides many built-in string methods and opraturs for manipulation :
+ 
+# indexing: : Access individual characters using brackets, starting at 0 for the first character (e.g., text[0]).
+# Slicing: Extract a portion of a string using text[start:end].Concatenation: Combine strings using the + operator.
+# Formatting: Use f-strings (e.g., f"Hello, {name}") to insert variables directly into strings.
+# Built-in Methods: Operations like .upper(), .lower(), .strip() (removing whitespace), and .replace(). 
+
+# Ex:
+
+text = "Hello, Kulalux"
+print(text[5])
+print(text.upper())
+print(len(text))
+
+
+

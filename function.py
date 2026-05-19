3cae993a8a266e556a5313a02e15bfa49be89479
rajranerr <raneraj71@gmail.com>
2026-05-19 15:10:37 +0530

    add new function

---
+++ function.py
@@ -0,0 +1,10 @@
+
+# Taking user input in python
+ # The input() function is a built-in tool that allows your program to receive data from the user through the keyboard.
+
+ # ex:
+
+name = input("enter your name: ")
+print("Hello,"+ name)
+
+

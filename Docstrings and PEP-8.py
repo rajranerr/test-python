d5d9000402d586d20dcbc68749d3422d843cbb23
rajranerr <raneraj71@gmail.com>
2026-05-25 14:36:30 +0530

    add Docstrings and PEP 8

---
+++ Docstrings and PEP-8.py
@@ -0,0 +1,10 @@
+# Docstrings: Python docstring are the stringb literrals that appear night after the defininmg of a function, method, class or module.
+# Ex:
+def square(n):
+    """"takes in a number n, returns the squer of n"""
+    print(n**2)
+square(6)
+print(square.__doc__)
+
+# PEP 8: PEP 8 is the official Style Guide for Python Code, providing a set of conventions to ensure Python code is readable, consistent, and "Pythonic".
+#         Originally written in 2001 by Guido van Rossum, Barry Warsaw, and Nick Coghlan, it centers on the principle that "code is read much more often than it is written".

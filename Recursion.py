8768405d24ce28f90913bdc50ab7da9074fc463c
rajranerr <raneraj71@gmail.com>
2026-05-26 16:51:52 +0530

    add new programs

--- Recursion.py
+++ Recursion.py
@@ -17,3 +17,29 @@ def factorial(num):
 num = 7;
 print("number: ",num)
 print("Factorial: ",factorial(num))
+
+# Ex:
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
+    
+print("fibonicci: ",fibonacci(20))
+
+# Ex:
+def tail_fact(n, acc=1):
+    if n == 0:
+        return acc
+    else:
+        return tail_fact(n-1, acc * n)
+    
+def nontail_fact(n):
+    if n == 0:
+        return 1
+    else:
+        return n * nontail_fact(n-1)
+print("tail_fact: ",tail_fact(10))
+print("nontail_fact: ",nontail_fact(10))

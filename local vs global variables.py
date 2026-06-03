33ea77961e31bfa2c3577103852d028d7e6ace8f
rajranerr <raneraj71@gmail.com>
2026-06-03 13:15:58 +0530

    one folder is deleted and add new program also

---
+++ local vs global variables.py
@@ -0,0 +1,7 @@
+# local and global: Local variables are defined inside a function and can only be accessed within that specific function, while global variables are defined outside of all functions and are accessible throughout the entire program.
+
+# global variable: Declared outside of all functions. Exists until the entire program ends. Accesseble anywherw in the program. Shared across all functions easily.
+# local variable: Declered inside a specific function. Created on function call; destroyed on return. Accessible only within its own function. Not shared with other function.
+
+# Local Variables: A local varible is isolated inside its function environment. if yo try to access a local variable from outside its function, Python will raise a NameError.
+

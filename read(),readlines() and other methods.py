0dd73630ba5c610b173732ad0c24c102d0c34100
rajranerr <raneraj71@gmail.com>
2026-06-03 16:19:50 +0530

    add new programs

--- read(),readlines() and other methods.py
+++ read(),readlines() and other methods.py
@@ -6,3 +6,17 @@ while True:
     if not line:
         break
     print(line)
+
+# Readlines() method: The readlines() method reads all the lines of the file and return them as a list of strings.
+# Ex:
+f =open('example.txt', 'r')
+line = f.readlines()
+print(line)
+f.close()
+
+# Writelines() method: The writeline() method in python write a squence of string to a file. The sequence can be any iterable object, such as a list or a tuple.
+# Ex:
+f = open('example.txt', 'w')
+lines = ['My name is Raj Rane\n', 'My father is a farmer\n', 'Hunter 350 and interseptor 650 are my favorite bikes\n']
+f.writelines(lines)
+f.close()

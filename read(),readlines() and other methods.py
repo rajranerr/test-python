05eb60f863d2159f625ec779c2a68a84355af9e1
rajranerr <raneraj71@gmail.com>
2026-06-03 16:35:35 +0530

    add new program

--- read(),readlines() and other methods.py
+++ read(),readlines() and other methods.py
@@ -20,3 +20,30 @@ f = open('example.txt', 'w')
 lines = ['My name is Raj Rane\n', 'My father is a farmer\n', 'Hunter 350 and interseptor 650 are my favorite bikes\n']
 f.writelines(lines)
 f.close()
+
+# readable(): Returns a boolean (True/False) indicating whether the file can be extracted from (e.g., if it was opened in 'r' or 'r+' mode).
+
+# writable(): Returns a boolean indicating whether the file can accept incoming data modifications (e.g., if opened in 'w' or 'a' mode).
+
+# Reading Specific Parts of a File:Sometimes, we may only need to read a specific part of a file, such as the first few bytes, a specific line, or a range of lines.
+# Ex:
+# file = open("example.txt","r")
+# content = file.read(10)
+# print(content)
+# file.close()
+
+# Reading CSV Files:CSV (Comma-Separated Values) is widely used for storing tabular data. Python’s csv module helps parse CSV easily. Here, instead of needing an external file, we’ll simulate one using io.StringIO
+# Ex: 
+import csv
+import io
+
+# Create a CSV sample in memory
+csv_data = """Year,Industry,value
+2014,Manufacturing,769200
+2014,Manufacturing,48200
+2014,Menufacturing,16
+"""
+csvfile = io.StringIO(csv_data)
+csvreader = csv.reader(csvfile)
+for row in csvreader:
+    print(row)

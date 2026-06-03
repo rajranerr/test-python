30b5e7c3cc3864750ed33b1028ba6d121a399ace
rajranerr <raneraj71@gmail.com>
2026-06-03 17:14:29 +0530

    add new programs

--- seek(),tell() and other functions.py
+++ seek(),tell() and other functions.py
@@ -10,3 +10,40 @@ with open('myfile.txt', 'r') as f:
     # Read the next 5 bytes
     data = f.read(8)
     print(data)
+
+f.close()
+
+# Ex 1: Using seek() in Text Mode
+
+f = open("myfile.txt", "r")
+
+f.seek(8)
+print(f.tell())
+print(f.readline())
+f.close()
+
+# Ex 2: Using seek() in Binary Mode with Negative Offset
+f = open("myfile.txt", "rb")
+f.seek(-10, 2)
+print(f.tell())
+print(f.readline().decode('utf-8'))
+f.close()
+
+# tell() function: The tell() method in Python returns the current position of the file pointer (or cursor) within an open file stream.
+
+with open("myfile.txt", "r") as f:
+    print(f.tell())
+    data = f.read(4)
+    print(data)
+    print(f.tell())
+f.close()
+
+# Combining tell() with seek(): The tell() method goes hand-in-hand with seek(), which moves the cursor manually.
+# Ex:
+with open("myfile.txt", "rb") as f:
+    f.read(9)   # Advance 9 bytes
+    saved_position = f.tell() # Bookmark this spot
+    print(saved_position)
+    f.read()   # Keep reading further ahead
+    f.seek(saved_position) # Telepport right back to the boolmarked spot
+f.close()

64292a328299459dbf9cc1525f8b69f101131b68
rajranerr <raneraj71@gmail.com>
2026-06-03 14:37:57 +0530

    add new two files

---
+++ File IO.py
@@ -0,0 +1,5 @@
+# Opening a File: Before we can perform any operations on a file, we must first open it. Python provides the open() function to open a file. It takes two arguments: the name of the file and the mode in which The file should be opened. The mode can be 'r' for reading, 'w' for writing, or 'a' for appending.
+
+# Example:
+# Here's an example of how to open a file for reading:
+f = open('','r')

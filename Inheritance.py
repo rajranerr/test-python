fa298220d75066a4d68ea5a9c8982ad8fd856c9a
rajranerr <raneraj71@gmail.com>
2026-06-05 14:09:44 +0530

    add more programs

--- Inheritance.py
+++ Inheritance.py
@@ -86,3 +86,87 @@ s1 = Son()
 s1.fathername = "Rajesh"
 s1.mothername = "Kavya"
 s1.parents()
+
+# Multilevel Inheritance
+# Ex:
+# Base class
+class Grandfather:
+    def __init__(self, grandfathername):
+        self.grandfathername = grandfathername
+
+# Intermediate class
+class Father(Grandfather):
+    def __init__(self, fathername, grandfathername):
+        self.fathername = fathername
+        # Call the constructor of Grandfather
+        Grandfather.__init__(self, grandfathername)
+
+# Derived class
+class Son(Father):
+    def __init__(self, sonname, fathername, grandfathername):
+        self.sonname = sonname
+        # Call the constructor of Father
+        Father.__init__(self, fathername, grandfathername)
+
+    def print_name(self):
+        print('Grandfather name :', self.grandfathername)
+        print('Father name :', self.fathername)
+        print('Son name :', self.sonname)
+
+# Driver code
+s1 = Son('Aditya', 'Rajesh', 'Devendra')
+print(s1.grandfathername)
+s1.print_name()
+
+# Hierarchical Inheritance
+# Ex:
+# Base class
+class Parent:
+    def func1(self):
+        print("This function is in parent class.")
+
+# Derived class 1
+class Child1(Parent):
+    def func2(self):
+        print("This function is in child 1.")
+
+# Derived class 2
+class Child2(Parent):
+    def func3(self):
+        print("This function is in child 2.")
+
+# Driver code
+object1 = Child1()
+object2 = Child2()
+
+object1.func1()
+object1.func2()
+object2.func1()
+object2.func3()
+
+#  Hybrid Inheritance
+# Ex:
+# Base class
+class School:
+    def func1(self):
+        print("This function is in school.")
+
+# Derived class 1 (Single Inheritance)
+class Student1(School):
+    def func2(self):
+        print("This function is in student 1.")
+
+# Derived class 2 (Another Single Inheritance)
+class Student2(School):
+    def func3(self):
+        print("This function is in student 2.")
+
+# Derived class 3 (Multiple Inheritance)
+class Student3(Student1, School):
+    def func4(self):
+        print("This function is in student 3.")
+
+# Driver code
+obj = Student3()
+obj.func1()
+obj.func2()
